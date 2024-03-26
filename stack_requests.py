8fee787f739188ea3cb2d3db54e9e9f0139defcd
victorien <victorienmichel3@gmail.com>
2024-03-26 16:22:11 +0100

    Request 1000 questions

--- stack_requests.py
+++ stack_requests.py
@@ -1,25 +1,28 @@
 from stackapi import StackAPI
 from datetime import datetime
-from pprint import pprint
 import json
+from pprint import pprint
 
 SITE = StackAPI('stackoverflow')
 
-questions = SITE.fetch(
+SITE.page_size = 100
+SITE.max_pages = 10
+
+response = SITE.fetch(
     'questions',
-    fromdate=datetime(2022, 1, 1),
+    fromdate=datetime(2010, 1, 1),
     todate=datetime.today(),
     min=50,
     tagged='python',
-    sort='votes'
+    sort='votes',
 )
 
-items = dict()
+questions = dict()
 
-for question_data in questions["items"][-50:]:
-    items[question_data["question_id"]] = dict()
-    items[question_data["question_id"]]["sentence"] = question_data["title"]
-    items[question_data["question_id"]]["tags"] = question_data["tags"]
+for question_data in response['items']:
+    questions[question_data["question_id"]] = dict()
+    questions[question_data["question_id"]]["sentence"] = question_data["title"]
+    questions[question_data["question_id"]]["tags"] = question_data["tags"]
 
 with open("data/questions.json", "w") as f:
-    json.dump(items, f)
+    json.dump(questions, f)
